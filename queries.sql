6784d89708f51dd5fb31ddc3e60b98c201ddbffe
Eman Fatima <emanfatima.37303@gmail.com>
2026-05-24 21:43:57 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,8 @@
+use schooldb;
+-- show all students
+select * from students;
+-- show only name and email
+select first_name,last_name,email from students;
+-- sort students by first name
+select * from students order by first_name asc;
 
