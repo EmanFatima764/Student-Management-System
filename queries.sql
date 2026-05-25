7e5d0fd53eca51fb63de57f1b9bf090b7dd43338
Eman Fatima <emanfatima.37303@gmail.com>
2026-05-25 08:19:45 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,17 @@
 use schooldb;
+
 -- show all students
 select * from students;
+
 -- show only name and email
 select first_name,last_name,email from students;
+
 -- sort students by first name
 select * from students order by first_name asc;
+
+--  Count Total Students
+SELECT COUNT(*) AS TOTAL_STUDENTS FROM STUDENTS;
+
+--  Find Number of Students in Each Department
+select dep_name , count(*)as total_student from departments group by dep_name;
 
